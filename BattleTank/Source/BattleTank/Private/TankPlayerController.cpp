c2c0ea2daee5ba7892f77660ccf88262e6bf8065
enrico <degan.enrico@gmail.com>
2020-02-26 05:42:28 -0500

    BT204 ATank class problems fixed

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay exists"))
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
