89667ea6d9c3427fe52a6770a5e5514f831d293c
enrico <degan.enrico@gmail.com>
2020-06-04 18:34:45 -0400

    BT228 Challange - Turret Rotation pt1 (v1)

---
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTurret.generated.h"
+
+
+UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+class BATTLETANK_API UTankTurret : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	void Yaw(float); // TODO add float name
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 5; // TODO change magic number
+};
